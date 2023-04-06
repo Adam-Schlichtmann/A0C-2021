db2d569452f9b028439865da11ac687569a4358f
Adam Schlichtmann <aschlichtmann@dmsi.com>
2023-04-05 19:57:31 -0500

    main file

---
+++ template/main.py
@@ -0,0 +1,20 @@
+def parseInput():
+  f = open("./src/dayXX/input.txt", "r")
+  l = list(map(lambda x: x, filter(lambda x: x, f.read().split("\n"))))
+  return l
+
+
+def part1():
+  print('Day 1 part 1')
+  lines = parseInput()
+
+
+def part2():
+  print('Day 1 part 2')
+  lines = parseInput()
+
+def main():
+  part1()
+  part2()
+
+main()
