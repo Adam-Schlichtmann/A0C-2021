fd4b752c11e7729d781717633044e1fa859ea03b
Adam Schlichtmann <aschlichtmann@dmsi.com>
2023-04-05 19:30:06 -0500

    set up for use

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import sys 
+
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+
+if (len(sys.argv) != 2):
+  print(f"{colors.FAIL}Invalid options.\n{colors.WARNING}Usage:`python3 main.py [day]`")
+os.system("python3 ./src/day{:02d}/main.py".format(int(sys.argv[1])))
